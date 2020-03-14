b4b49cba1df17bee4b2bb1d61c2fc55c191eb4e7
Brian <brianhh1230@gmail.com>
2020-03-14 12:53:39 -0400

    adding index config for docs build

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,9 @@ author = "Brian Hopkins"
 # ones.
 extensions = []
 
+
+master_doc = 'index'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
