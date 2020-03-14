5cc1e4d7bc34728c1cbd677c35a105a473466226
Brian <brianhh1230@gmail.com>
2020-03-14 07:39:38 -0400

    found new cookiecutter that has what I need, so initial file upload

---
+++ serpent_tracker/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from django.test import RequestFactory
+
+from serpent_tracker.users.models import User
+from serpent_tracker.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
+
+
+@pytest.fixture
+def request_factory() -> RequestFactory:
+    return RequestFactory()
